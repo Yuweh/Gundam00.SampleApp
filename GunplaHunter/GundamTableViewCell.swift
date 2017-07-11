a0c004dc2ad3b343152f1204539be979ce7f10c4
Francis Jemuel Bergonia <francisyuweh@Franciss-MacBook-Pro.local>
2017-07-11 23:46:03 +0800

    Gundam Table View Cell for MS units on the works :D

---
+++ GunplaHunter/GundamTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  GundamTableViewCell.swift
+//  GunplaHunter
+//
+//  Created by Francis Jemuel Bergonia on 11/07/2017.
+//  Copyright © 2017 Francis Jemuel Bergonia. All rights reserved.
+//
+
+import UIKit
+
+class GundamTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
